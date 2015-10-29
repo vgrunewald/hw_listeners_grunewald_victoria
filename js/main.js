0b0756ce748c4e46a3e7f931897b27f0478379fb
Vicki Grunewald <vicki.grunewald@gmail.com>
2015-10-29 14:53:24 -0700

    Add double click event to change top heading

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ var background = document.body;
 background.addEventListener("click", function(){
     this.style.backgroundColor = "#33f200";
 });
+
+//#2: Event listener listens for double click on top page heading. Upon double click, listener changes top heading to new text.
+var heading = document.getElementById("page-title");
+heading.addEventListener("dblclick", function() {
+    this.innerHTML="Go Seahawks!";
+});
+
